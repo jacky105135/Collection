32b8ade460de0a3b332ed5ac21e1ed7186e6fbef
user <user@DESKTOP-7A77O9H>
2018-09-27 23:55:19 +0800

    done1

--- src/list/sorting.java
+++ src/list/sorting.java
@@ -8,24 +8,23 @@ public class sorting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner s = new Scanner(System.in);
-		List list = Arrays.asList(s.nextLine());
-		String t1 = list.toString();
+		List l = Arrays.asList(s.nextLine());
+		String t1 = l.toString();
 		String a = t1.replace("[", "");
 		String b = a.replace("]", "");
 		String[] c = b.split(" ");
-		
 
 		for (int i = 0; i < c.length; i++) {
-			if(c.length%2 != 0){
-				if(i != 0 && i % 2 != 0){
-					System.out.print(c[c.length-(i+1)] + " ");
-			}
-			}else{
-				if(i != 0 && i % 2 != 0){
-					System.out.print(c[c.length-(i)] + " ");
-			}
+			if (c.length % 2 != 0) {
+				if (i % 2 != 0) {
+					System.out.print(c[c.length - (i + 1)] + " ");
+				}
+			} else {
+				if (i % 2 != 0) {
+					System.out.print(c[c.length - i] + " ");
+				}
 			}
 		}
 	}
